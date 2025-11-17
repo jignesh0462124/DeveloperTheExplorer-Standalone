2c7e0e288c851dced9151540c22edfe0eb569db7
Kunal Nibrad <kunalnibrad297@gmail.com>
2025-11-17 23:16:01 +0530

    signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,19 @@ import "./App.css";
 import EventPoster from "./EventPoster";
 import Checkout from "./Checkout";
 import LandingPage from "./components/LandingPage";
-import Signup from "./dashboard/Signup";
+import Signup from "./dashboard/signup";
+import {Routes,Route,BrowserRouter} from "react-router"
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Signup />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+    </BrowserRouter>
     </>
   );
 }
